0ab18692c9a915f031f37a867d3a74a1c68bd139
Miami <alex.miami.petrov@gmail.com>
2024-09-03 10:10:23 +0300

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,80 @@
+// import React, { useState, useEffect } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+
+// import Feedback from "./components/Feedback/Feedback";
+// import Options from "./components/Options/Options";
+// import Notification from "./components/Notification/Notification";
+// import Description from "./components/Description/Description";
+
+// const App = () => {
+//   const [feedback, setFeedback] = useState({
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   });
+
+//   const updateFeedback = (feedbackType) => {
+//     setFeedback({
+//       ...feedback,
+//       [feedbackType]: feedback[feedbackType] + 1,
+//     });
+//   };
+
+//   const resetFeedback = () => {
+//     setFeedback({
+//       good: 0,
+//       neutral: 0,
+//       bad: 0,
+//     });
+//   };
+
+//   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+//   const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
+
+//   useEffect(() => {
+//     const storedFeedback = JSON.parse(localStorage.getItem("feedback"));
+//     if (storedFeedback) {
+//       setFeedback(storedFeedback);
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     localStorage.setItem("feedback", JSON.stringify(feedback));
+//   }, [feedback]);
+
+//   return (
+//     <div>
+//       <Description />
+//       <Options
+//         updateFeedback={updateFeedback}
+//         totalFeedback={totalFeedback}
+//         resetFeedback={resetFeedback}
+//       />
+//       {totalFeedback > 0 ? (
+//         <Feedback feedback={feedback} positiveFeedback={positiveFeedback} />
+//       ) : (
+//         <Notification message="Please leave your feedback by selecting one of the options below." />
+//       )}
+//       <p>Total feedback collected: {totalFeedback}</p>
+//       <p>
+//         Percentage of positive feedback:{" "}
+//         {isNaN(positiveFeedback) ? "0%" : `${positiveFeedback}%`}
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default App;
+
 import React, { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 import Feedback from "./components/Feedback/Feedback";
 import Options from "./components/Options/Options";
 import Notification from "./components/Notification/Notification";
+import Description from "./components/Description/Description"; // Import the Description component
 
 const App = () => {
   const [feedback, setFeedback] = useState({
@@ -15,10 +84,10 @@ const App = () => {
   });
 
   const updateFeedback = (feedbackType) => {
-    setFeedback({
-      ...feedback,
-      [feedbackType]: feedback[feedbackType] + 1,
-    });
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
   };
 
   const resetFeedback = () => {
@@ -30,7 +99,8 @@ const App = () => {
   };
 
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-  const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
+  const positiveFeedbackPercentage =
+    totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
 
   useEffect(() => {
     const storedFeedback = JSON.parse(localStorage.getItem("feedback"));
@@ -45,22 +115,23 @@ const App = () => {
 
   return (
     <div>
-      <h1>Sip Happens Café</h1>
+      <Description
+        totalFeedback={totalFeedback}
+        positiveFeedbackPercentage={positiveFeedbackPercentage}
+      />
       <Options
         updateFeedback={updateFeedback}
         totalFeedback={totalFeedback}
         resetFeedback={resetFeedback}
       />
       {totalFeedback > 0 ? (
-        <Feedback feedback={feedback} positiveFeedback={positiveFeedback} />
+        <Feedback
+          feedback={feedback}
+          positiveFeedbackPercentage={positiveFeedbackPercentage}
+        />
       ) : (
         <Notification message="Please leave your feedback by selecting one of the options below." />
       )}
-      <p>Total feedback collected: {totalFeedback}</p>
-      <p>
-        Percentage of positive feedback:{" "}
-        {isNaN(positiveFeedback) ? "0%" : `${positiveFeedback}%`}
-      </p>
     </div>
   );
 };
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,12 +1,37 @@
+// import React from "react";
+
+// const Feedback = ({ feedback }) => {
+//   return (
+//     <div>
+//       <h2>Feedback statistics</h2>
+//       <p>Good: {feedback.good}</p>
+//       <p>Neutral: {feedback.neutral}</p>
+//       <p>Bad: {feedback.bad}</p>
+//     </div>
+//   );
+// };
+
+// export default Feedback;
+
 import React from "react";
 
-const Feedback = ({ feedback }) => {
+const Feedback = ({ feedback, positiveFeedbackPercentage }) => {
+  const { good, neutral, bad } = feedback;
+  const totalFeedback = good + neutral + bad;
+
   return (
     <div>
-      <h2>Feedback statistics</h2>
-      <p>Good: {feedback.good}</p>
-      <p>Neutral: {feedback.neutral}</p>
-      <p>Bad: {feedback.bad}</p>
+      <h2>Feedback Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total feedback collected: {totalFeedback}</p>
+      <p>
+        Percentage of positive feedback:{" "}
+        {isNaN(positiveFeedbackPercentage)
+          ? "0%"
+          : `${positiveFeedbackPercentage}%`}
+      </p>
     </div>
   );
 };
